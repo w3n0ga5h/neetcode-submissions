f81c093a07509447bbe35ae8671ace0744fc1176
Yanis SADI <173709078+w3n0ga5h@users.noreply.github.com>
2026-07-26 17:08:01 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 2.7
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "test"
+print(type(variable))
+variable = [1,2]
+print(type(variable))
