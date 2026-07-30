7f9e29afbd84d0889278971327b89cd210d663bc
Yanis SADI <173709078+w3n0ga5h@users.noreply.github.com>
2026-07-30 21:51:26 +0100

    Add: python-set-operations - submission-2

---
+++ Python For Beginners/python-set-operations/submission-2.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def contains_duplicate(words: List[str]) -> bool:
+    myset=set()
+    for elements in words:
+        myset.add(elements)
+        if myset:
+            continue
+        else:
+            return 0
+    return len(myset)
+# do not modify code below this line
+print(contains_duplicate(["hello", "world", "hello"]))
+print(contains_duplicate(["hello", "world", "i", "am", "great"]))
+print(contains_duplicate(["hello", "hello", "hello"]))
+print(contains_duplicate(["Hello", "hellooo", "hello"]))
