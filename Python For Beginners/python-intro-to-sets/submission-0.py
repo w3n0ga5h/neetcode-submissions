4471dcffb0a2ccf6c18a1cc09360d4d4f2f21baf
Yanis SADI <173709078+w3n0ga5h@users.noreply.github.com>
2026-07-30 21:42:25 +0100

    Add: python-intro-to-sets - submission-0

---
+++ Python For Beginners/python-intro-to-sets/submission-0.py
@@ -0,0 +1,12 @@
+from typing import List, Set # this adds type hints for List and Set
+
+def list_to_set(nums: List[int]) -> Set[int]:
+    myset = set()
+    for i in nums:
+        myset.add(i)
+    return myset
+
+# do not modify below this line
+print(list_to_set([1, 2, 3, 4, 5]))
+print(list_to_set([1, 1, 2, 2, 3, 3]))
+print(list_to_set([1, 2, 3, 4, 5, 5, 5, 3, 4, 5]))
