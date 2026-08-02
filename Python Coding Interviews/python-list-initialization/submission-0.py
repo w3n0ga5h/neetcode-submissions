373603bee7ce9d7e058e27bcfe64e6575c9fb4b2
Yanis SADI <173709078+w3n0ga5h@users.noreply.github.com>
2026-08-02 20:27:46 +0100

    Add: python-list-initialization - submission-0

---
+++ Python Coding Interviews/python-list-initialization/submission-0.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def create_list_with_value(size: int, index: int, value: int) -> List[int]:
+    list_a=[0]*size
+    list_a[index]=value
+    return list_a
+    
+
+
+
+# do not modify below this line
+print(create_list_with_value(5, 3, 7))
+print(create_list_with_value(1, 0, 5))
+print(create_list_with_value(10, 9, 9))
+print(create_list_with_value(10, 9, 0))
