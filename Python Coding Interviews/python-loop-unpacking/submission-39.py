f74869e2d112826b7e48bade6939108f3fb363bc
Yanis SADI <173709078+w3n0ga5h@users.noreply.github.com>
2026-08-02 17:00:38 +0100

    Add: python-loop-unpacking - submission-39

---
+++ Python Coding Interviews/python-loop-unpacking/submission-39.py
@@ -0,0 +1,22 @@
+from typing import List, Tuple
+
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    listfinal=0
+    for x, y in scores:
+        print (x,y)
+        if y > listfinal:
+            print ("if y > list final",x,y)
+            high=x
+            print (x)
+            listfinal=y
+        else:
+            continue
+ #           name = x
+ #           print (name,listfinal)
+#    return (x)
+    
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
