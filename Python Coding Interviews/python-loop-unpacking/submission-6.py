28a15d9184695b842a7ff9b090a4577fdbe64990
Yanis SADI <173709078+w3n0ga5h@users.noreply.github.com>
2026-08-02 16:35:28 +0100

    Add: python-loop-unpacking - submission-6

---
+++ Python Coding Interviews/python-loop-unpacking/submission-6.py
@@ -0,0 +1,12 @@
+from typing import List, Tuple
+
+a= 0
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    for x,y in scores:
+        final =[x,y]
+    print (final)
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
