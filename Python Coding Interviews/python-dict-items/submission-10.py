181d8780dc1fb10d53179c8f3579deaa643ce040
Yanis SADI <173709078+w3n0ga5h@users.noreply.github.com>
2026-08-03 19:33:29 +0100

    Add: python-dict-items - submission-10

---
+++ Python Coding Interviews/python-dict-items/submission-10.py
@@ -0,0 +1,16 @@
+from typing import Dict, List, Tuple
+
+
+def get_dict_items(age_dict: Dict[str, int]) -> List[Tuple[str, int]]:
+    name=age_dict.keys()
+    name_list=list(name)
+    return name_list
+    age=age_dict.values()
+    agelist=list(age)
+    return agelist
+
+# do not modify below this line
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}))
+print(get_dict_items({'Bob': 30, 'David': 40, 'Charlie': 35, 'Alice': 25, 'Eve': 45}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40, 'Eve': 45, 'Frank': 50}))
