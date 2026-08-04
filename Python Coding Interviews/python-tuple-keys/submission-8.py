a61b279430096e32cf74cc286dd69a00444575cd
Yanis SADI <173709078+w3n0ga5h@users.noreply.github.com>
2026-08-04 23:51:26 +0100

    Add: python-tuple-keys - submission-8

---
+++ Python Coding Interviews/python-tuple-keys/submission-8.py
@@ -0,0 +1,25 @@
+from typing import List, Set, Tuple
+
+
+def grid_to_set(grid: List[List[int]]) -> Set[Tuple[int, int]]:
+    for x in grid:
+        print(x[0])
+
+
+# do not modify below this line
+grid_to_set([[1, 0, 1], [0, 1, 0], [1, 0, 1]])
+#output1 = grid_to_set([[1, 0, 1], [0, 1, 0], [1, 0, 1]])
+#print(type(output1))
+#print(sorted(list(output1)))
+      
+#output2 = grid_to_set([[1, 0, 0], [0, 0, 0]])
+#print(type(output2))
+#print(sorted(list(output2)))
+
+#output3 = grid_to_set([[1, 1, 1], [1, 1, 1]])
+#print(type(output3))
+#print(sorted(list(output3)))
+
+#output4 = grid_to_set([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+#print(type(output4))
+#print(sorted(list(output4)))
