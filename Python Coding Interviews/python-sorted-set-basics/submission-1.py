5946d9e042a104a349c7717413efcd73ddbe306f
Yanis SADI <173709078+w3n0ga5h@users.noreply.github.com>
2026-08-05 23:47:24 +0100

    Add: python-sorted-set-basics - submission-1

---
+++ Python Coding Interviews/python-sorted-set-basics/submission-1.py
@@ -0,0 +1,18 @@
+from typing import List
+from sortedcontainers import SortedSet
+
+
+def get_first_three(sorted_set: SortedSet[int], nums1: List[int], nums2: List[int]) -> List[int]:
+    listing=[]
+    for x in nums1:
+        sorted_set.add(x)
+        print(sorted_set)
+    for a in nums2:
+        sorted_set.pop(a)
+    return listing
+
+# do not modify below this line
+#print(get_first_three(SortedSet(), [1, 2, 3], [4]))
+#print(get_first_three(SortedSet([1, 4, 7, 2, 8, 9]), [10], [1, 7, 2]))
+#print(get_first_three(SortedSet([1, 2, 3, 7]), [], [4, 5, 6]))
+#print(get_first_three(SortedSet([1, 2, 3, 4, 5, 6, 7, 8, 9]), [10, 11, 12], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
