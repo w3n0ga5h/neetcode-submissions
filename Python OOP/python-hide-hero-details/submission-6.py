4b08d2da06f491944b6727b13ed9e4d4a982cd14
Yanis SADI <173709078+w3n0ga5h@users.noreply.github.com>
2026-08-16 16:05:45 +0100

    Add: python-hide-hero-details - submission-6

---
+++ Python OOP/python-hide-hero-details/submission-6.py
@@ -0,0 +1,45 @@
+class SuperHero:
+    def __init__(self, name: str, health: int, power_level: int):
+        self.name = name
+        # TODO: Add the private attributes
+        self.__health = health
+        self.__power = power_level
+    # TODO: Add the getter and setter methods
+
+    def get_health(self):
+        return self.__health
+
+    def set_health(self,new_health:int):
+        if 0 >= new_health <= 100:
+            self.__health= new_health
+        elif new_health > 100:
+            print("You can't set the health to more than 100")
+        else:
+            print("You can't set the health to less than 0")
+
+    def get_power_level(self):
+        return self.__power
+    def set_power_level(self,new_power:int):
+        if 1 >= new_power <= 10:
+            self.__power= new_power
+        elif new_power > 10:
+            print("You can't set the power to more than 10")
+        else:
+            print("You can't set the power to less than 1")
+
+super_hero = SuperHero("Batman", 80, 9)
+
+print(super_hero.get_health()) # this should print 80
+super_hero.set_health(110) # this should print You can't set the health to more than 100
+super_hero.set_health(-10) # this should print You can't set the health to less than 100
+super_hero.set_health(70)
+
+print(super_hero.get_power_level()) # this should print 9
+super_hero.set_power_level(11) # this should print You can't set the power level to more than 10
+super_hero.set_power_level(0) # this should print You can't set the power level to less than 1
+super_hero.set_power_level(7)
+
+
+
+# TODO: print the hero's attributes
+print((super_hero.name),"has",(super_hero.get_health())," health and ",(super_hero.get_power_level())," power level.")
